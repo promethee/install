f97a56b896ef17255706849d42d768958b9a7c3a
promethee <lunette.francois@gmail.com>
2021-02-17 18:18:22 +0100

    Create coral.usb.sh

---
+++ coral.usb.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y libedgetpu1-std python3-pycoral
+mkdir coral && cd coral
+git clone https://github.com/google-coral/pycoral.git
+cd pycoral
+bash examples/install_requirements.sh classify_image.py
+python3 examples/classify_image.py --model test_data/mobilenet_v2_1.0_224_inat_bird_quant_edgetpu.tflite --labels test_data/inat_bird_labels.txt --input test_data/parrot.jpg
