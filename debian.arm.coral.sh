f15b27b5ab58fcc80e988612b7c6f400ab04d58d
promethee <lunette.francois@gmail.com>
2021-02-18 16:29:59 +0100

    Update debian.arm.coral.sh

--- debian.arm.coral.sh
+++ debian.arm.coral.sh
@@ -2,8 +2,7 @@
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install -y python3-wheel libedgetpu1-std python3-pycoral
-wget https://dl.google.com/coral/python/tflite_runtime-2.1.0.post1-cp36-cp36m-linux_armv7l.whl
+sudo apt-get install -y python3-wheel libedgetpu1-std python3-edgetpu python3-pycoral
 mkdir coral && cd coral
 git clone https://github.com/google-coral/pycoral.git
 cd pycoral
